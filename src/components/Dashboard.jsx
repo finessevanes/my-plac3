7dcc5b68d93520e20883e9223afeff1b3b976652
Vanessa Mercado <finessevanes@Vanessas-MacBook-Pro.local>
2022-08-21 00:27:57 -0500

    chore: sucks for screen readers

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,28 +7,28 @@ import PublicationCard from './PublicationCard'
 
 const Dashboard = () => {
   const { address } = useAccount()
-  if (address) {
-    return (
-      <div style={{ border: '2px solid red', textAlign: 'center', flexDirection: 'column' }}>
-        <WorldIDWidget
-          actionId="wid_staging_b7f41eda52d89baab9bd3bb99fb38d4a" // sofie's
-          signal="my_signal"
-          enableTelemetry
-          onSuccess={(verificationResponse) => console.log(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
-          onError={(error) => console.error(error)}
-        />
-        <p>Welcome!
-          Connect your wallet to see recommendations curated by your trusted communities</p>
-        <Profiles />
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <p> No account</p>
-      </>
-    );
-  }
+
+  return (
+    <>
+      {address && (
+        <div style={{ border: '2px solid red', textAlign: 'center', flexDirection: 'column' }}>
+          <WorldIDWidget
+            actionId="wid_staging_b7f41eda52d89baab9bd3bb99fb38d4a" // sofie's
+            signal="my_signal"
+            enableTelemetry
+            onSuccess={(verificationResponse) => console.log(verificationResponse)} // you'll actually want to pass the proof to the API or your smart contract
+            onError={(error) => console.error(error)}
+          />
+          <p>Welcome!
+            Connect your wallet to see recommendations curated by your trusted communities</p>
+          <Profiles />
+        </div>
+      )}
+      {!address && (
+        <h1>No wallet</h1>
+      )}
+    </>
+  )
 };
 
 export default Dashboard;
